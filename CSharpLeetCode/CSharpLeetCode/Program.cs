d509f628efc1f66d152d940c115de95a01d5a0cd
Dogtwo <454186556@QQ.com>
2020-06-13 20:15:21 +0800

    Initial and 1137

---
+++ CSharpLeetCode/CSharpLeetCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using CSharpLeetCode.Core;
+
+namespace CSharpLeetCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //使用对应的文件来跑，举个例子：
+            //Tribonacci_1137 solution = new Tribonacci_1137();
+            //solution.Tribonacci(25);
+        }
+    }
+}
